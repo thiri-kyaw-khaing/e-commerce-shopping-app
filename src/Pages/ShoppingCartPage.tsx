3a57b1c00a3f8c1d0617295b0555b7fd61cfca19
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-08-24 23:05:53 +0700

    card of cart item init

--- src/Pages/ShoppingCartPage.tsx
+++ src/Pages/ShoppingCartPage.tsx
@@ -1,6 +1,6 @@
 import { cartItems } from "@/data/cartItems";
 import type { cartItem } from "@/types";
-
+import { Checkbox } from "@/components/ui/checkbox";
 interface cartProps {
   items?: cartItem[];
 }
@@ -19,8 +19,16 @@ export default function ShoppingCartPage({ items = cartItems }: cartProps) {
         <p className="text-center">Your cart is empty</p>
       ) : (
         items.map((item) => (
-          <div className="border rounded-lg p-2 my-2" key={item.id}>
-            {item.name}
+          <div className="border rounded-lg p-2 my-2 flex gap-4" key={item.id}>
+            <div className="flex items-center  gap-6">
+              <Checkbox />
+              <img
+                src={item.image.url}
+                alt={item.name}
+                className="w-[100px] h-[100px] object-cover"
+              />
+            </div>
+            <div className="">{item.name}</div>
           </div>
         ))
       )}
