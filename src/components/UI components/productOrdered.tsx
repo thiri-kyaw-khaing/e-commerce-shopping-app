9e063280af5b89d7608fcf96de0e0742c523065b
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-08 23:57:44 +0700

    fix order product

--- src/components/UI components/productOrdered.tsx
+++ src/components/UI components/productOrdered.tsx
@@ -4,7 +4,20 @@ import { MapPin } from "lucide-react";
 export default function productOrdered() {
   return (
     <>
-      <div className="border p-6 space-y-6 font-bold">Product Ordered</div>
+      <div className="border p-6 space-y-6">
+        <div className="flex items-center gap-4 justify-between">
+          <span className="font-bold">Product Ordered</span>
+          <h1>Unit Price</h1>
+          <h1>Amount</h1>
+          <h1>Item Subtotal</h1>
+        </div>
+        <div className="flex items-center gap-4 justify-between">
+          <span className="font-bold">Product Ordered</span>
+          <h1>Unit Price</h1>
+          <h1>Amount</h1>
+          <h1>Item Subtotal</h1>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/data/order.ts
@@ -0,0 +1,6 @@
+export const orderedProduct = {
+  productName: "Modern Sofa",
+  unitPrice: 12000,
+  amount: 1,
+  itemSubtotal: 12000, // unitPrice * amount
+};
