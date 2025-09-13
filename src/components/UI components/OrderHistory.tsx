8eabfcd43d9ae73e7565adf8c578c7efa01fa301
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-13 19:32:38 +0700

    order card

--- src/components/UI components/OrderHistory.tsx
+++ src/components/UI components/OrderHistory.tsx
@@ -1,3 +1,18 @@
+import { exampleOrder } from "@/data/order";
+import { CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardFooter,
+} from "@/components/ui/card";
+import ProductOrderedCard from "@/components/UI components/productOrdered";
 export default function OrderHistory() {
-  return <div>Order history goes here...</div>;
+  return (
+    <div>
+      <ProductOrderedCard order={exampleOrder} />
+    </div>
+  );
 }
