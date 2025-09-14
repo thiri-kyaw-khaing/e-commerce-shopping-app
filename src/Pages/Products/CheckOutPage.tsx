ff0da2efd60209e10a66a31878e5c5adf2cca6db
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-14 16:03:57 +0700

    order history dummy data fix

--- src/Pages/Products/CheckOutPage.tsx
+++ src/Pages/Products/CheckOutPage.tsx
@@ -2,14 +2,14 @@ import type { OrderUser } from "@/types";
 import { MapPin, PlusCircleIcon } from "lucide-react";
 import OrderUserInfo from "@/components/UI components/orderUserInfo";
 import { orderUser } from "@/data/orderuser";
-import { exampleOrder } from "@/data/order";
+import { exampleOrders } from "@/data/order";
 import ProductOrdered from "@/components/UI components/productOrdered";
 import PaymentMethod from "@/components/UI components/paymentMethod";
 import { Button } from "@/components/ui/button";
 import { PlusIcon } from "@radix-ui/react-icons";
 
 export default function CheckOutPage() {
-  const totalAmount = exampleOrder.totalAmount;
+  const totalAmount = exampleOrders[0].totalAmount;
   const shippingFee = 5000;
   return (
     <>
@@ -19,7 +19,7 @@ export default function CheckOutPage() {
         <OrderUserInfo orderUser={orderUser} />
       </div>
       <div className="mt-6 space-y-10">
-        <ProductOrdered order={exampleOrder} />
+        <ProductOrdered order={exampleOrders[0]} />
         <PaymentMethod />
         <div className="border p-6 mt-6 flex-col items-center justify-center gap-6 text-center space-y-6 rounded-md">
           <Button className="bg-transparent hover:bg-gray-300 text-black font-normal w-30 h-30 border dotted">
--- src/data/order.ts
+++ src/data/order.ts
@@ -1,61 +1,63 @@
 import type { Order } from "@/types";
 
-export const exampleOrder: Order = {
-  orderId: "order123",
-  user: {
-    id: "user1",
-    fullName: "Thiri Kyaw Khaing",
-    phone: "+66 912345678",
-    address: "123 Chiang Rai Road, Mueang District",
-    city: "Chiang Rai",
-    postalCode: "57100",
-    country: "Thailand",
-  },
-  items: [
-    {
-      id: "ci1",
-      name: "Sneakers",
-      price: 199_999,
-      quantity: 1,
-      image: {
-        id: "img1",
-        name: "sneakers.jpg",
-        url: "/images/nikedunklow.jpg",
-      },
-      category: "Footwear",
+export const exampleOrders: Order[] = [
+  {
+    orderId: "order123",
+    user: {
+      id: "user1",
+      fullName: "Thiri Kyaw Khaing",
+      phone: "+66 912345678",
+      address: "123 Chiang Rai Road, Mueang District",
+      city: "Chiang Rai",
+      postalCode: "57100",
+      country: "Thailand",
     },
-  ],
-  totalAmount: 199999,
-  createdAt: new Date(),
-  status: "pending",
-};
+    items: [
+      {
+        id: "ci1",
+        name: "Sneakers",
+        price: 199_999,
+        quantity: 1,
+        image: {
+          id: "img1",
+          name: "sneakers.jpg",
+          url: "/images/nikedunklow.jpg",
+        },
+        category: "Footwear",
+      },
+    ],
+    totalAmount: 199999,
+    createdAt: new Date(),
+    status: "pending",
+  },
 
-export const exampleOrder2: Order = {
-  orderId: "order124",
-  user: {
-    id: "user2",
-    fullName: "Aung Myo Min",
-    phone: "+66 987654321",
-    address: "456 Mae Fah Luang Avenue, Wiang District",
-    city: "Chiang Rai",
-    postalCode: "57110",
-    country: "Thailand",
-  },
-  items: [
-    {
-      id: "ci2",
-      name: "Adidas Samba",
-      price: 220_000,
-      quantity: 1,
-      image: {
-        id: "img2",
-        name: "ultraboost.jpg",
-        url: "/images/samba.webp",
-      },
-      category: "Footwear",
+  {
+    orderId: "order124",
+    user: {
+      id: "user2",
+      fullName: "Aung Myo Min",
+      phone: "+66 987654321",
+      address: "456 Mae Fah Luang Avenue, Wiang District",
+      city: "Chiang Rai",
+      postalCode: "57110",
+      country: "Thailand",
     },
-  ],
-  totalAmount: 220_000,
-  createdAt: new Date(),
-  status: "paid",
-};
+    items: [
+      {
+        id: "ci2",
+        name: "Adidas Samba",
+        price: 220_000,
+        quantity: 1,
+        image: {
+          id: "img2",
+          name: "ultraboost.jpg",
+          url: "/images/samba.webp",
+        },
+        category: "Footwear",
+      },
+    ],
+    totalAmount: 220_000,
+    createdAt: new Date(),
+    status: "paid",
+  },
+];
