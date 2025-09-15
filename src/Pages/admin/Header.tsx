ed4d34eee363f07595adcc4b7a38b00595fb7b8f
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-15 22:14:58 +0700

    admin nav finish

--- src/Pages/admin/Header.tsx
+++ src/Pages/admin/Header.tsx
@@ -1,7 +1,16 @@
+import ProfileButton from "@/components/HeaderComponent/profile";
+import { Separator } from "@/components/ui/separator";
+import { User } from "@/data/user";
+
 export default function Header() {
   return (
     <>
-      <h1 className="text-xl font-bold">E-commerce</h1>
+      <header className="w-full border-b">
+        <div className="flex justify-between items-center flex-1 pr-20 h-16">
+          <div className="pl-20 font-bold">E-commerce</div>
+          <ProfileButton user={User} />
+        </div>
+      </header>
     </>
   );
 }
--- src/Pages/admin/rootlayout.tsx
+++ src/Pages/admin/rootlayout.tsx
@@ -4,10 +4,8 @@ import Header from "./Header";
 export default function RootLayoutAdmin() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <div className="container mx-auto px-4 py-8">
-        <Header />
-        <Outlet />
-      </div>
+      <Header />
+      <Outlet />
     </div>
     //     <>
     //   <div className="flex flex-col min-h-screen">
