4395ff0cdb1241921d11b41650a4b355cea15e75
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-20 21:27:13 +0700

    card component

--- src/Pages/admin/adminOverview.tsx
+++ src/Pages/admin/adminOverview.tsx
@@ -1,8 +1,18 @@
+import { DollarSignIcon } from "lucide-react";
+import CardComponent from "./components/cardComponent";
+
 export default function AdminOverview() {
   return (
     <>
-      <div className="bg-gray-400 text-black  rounded-xl p-4 mx-20">
-        <h1 className="text-xl font-semibold">Admin Overview</h1>
+      <div className="bg-gray-400 text-black rounded-xl p-4 mx-20">
+        <CardComponent
+          title="Total Revenue"
+          amount="120,000 MMK"
+          percentageChange="+5% from last month"
+          icon={
+            <DollarSignIcon className="h-6 w-6 text-green-500 inline-block ml-2" />
+          }
+        />
       </div>
     </>
   );
---
+++ src/Pages/admin/components/cardComponent.tsx
@@ -0,0 +1,26 @@
+import { DollarSignIcon } from "lucide-react";
+
+export default function CardComponent({
+  title,
+  amount,
+  percentageChange,
+  icon,
+}: {
+  title?: string;
+  amount?: string;
+  percentageChange?: string;
+  icon?: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="bg-white p-4 rounded-lg space-y-2 w-64 shadow-md">
+        <h2 className="text-lg font-semibold mb-2 text-gray-700">{title}</h2>
+        <div className="flex items-center">
+          <p className="text-2xl font-bold text-black">{amount}</p>
+          {icon}
+        </div>
+        <p className="text-sm text-green-500">{percentageChange}</p>
+      </div>
+    </>
+  );
+}
