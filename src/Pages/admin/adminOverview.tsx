070949869103fa17df1d607c68d2a0f27f33cd9a
thiri-kyaw-khaing <6631503093@lamduan.mfu.ac.th>
2025-09-20 21:36:00 +0700

    card finish

--- src/Pages/admin/adminOverview.tsx
+++ src/Pages/admin/adminOverview.tsx
@@ -1,16 +1,33 @@
-import { DollarSignIcon } from "lucide-react";
+import { BoxIcon, DollarSignIcon } from "lucide-react";
 import CardComponent from "./components/cardComponent";
+import { PersonIcon } from "@radix-ui/react-icons";
 
 export default function AdminOverview() {
   return (
     <>
-      <div className="bg-gray-400 text-black rounded-xl p-4 mx-20">
+      <div className="bg-gray-400 text-black rounded-xl p-4 mx-20 flex gap-6 items-center">
         <CardComponent
           title="Total Revenue"
           amount="120,000 MMK"
-          percentageChange="+5% from last month"
+          percentageChange="+5%"
           icon={
             <DollarSignIcon className="h-6 w-6 text-green-500 inline-block ml-2" />
+          }
+        />
+        <CardComponent
+          title="Total Orders"
+          amount="1,200"
+          percentageChange="-2%"
+          icon={
+            <BoxIcon className="h-6 w-6 text-green-500 inline-block ml-2" />
+          }
+        />
+        <CardComponent
+          title="Total Users"
+          amount="300"
+          percentageChange="+10% "
+          icon={
+            <PersonIcon className="h-6 w-6 text-green-500 inline-block ml-2" />
           }
         />
       </div>
--- src/Pages/admin/components/cardComponent.tsx
+++ src/Pages/admin/components/cardComponent.tsx
@@ -8,18 +8,26 @@ export default function CardComponent({
 }: {
   title?: string;
   amount?: string;
-  percentageChange?: string;
+  percentageChange?: string | number;
   icon?: React.ReactNode;
 }) {
   return (
     <>
       <div className="bg-white p-4 rounded-lg space-y-2 w-64 shadow-md">
         <h2 className="text-lg font-semibold mb-2 text-gray-700">{title}</h2>
-        <div className="flex items-center">
+        <div className="flex items-center justify-between">
           <p className="text-2xl font-bold text-black">{amount}</p>
           {icon}
         </div>
-        <p className="text-sm text-green-500">{percentageChange}</p>
+        <p
+          className={`text-sm ${
+            parseFloat(percentageChange) >= 0
+              ? "text-green-500"
+              : "text-red-500"
+          }`}
+        >
+          {percentageChange}
+        </p>
       </div>
     </>
   );
